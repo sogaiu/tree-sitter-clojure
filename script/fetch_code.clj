063365533a6ff1083b46278bdc5ea5dc40c70c74
sogaiu <983021772@users.noreply.github.com>
2023-02-27 14:01:07 +0900

    Add babashka bits for ClojureDart exploration

---
+++ script/fetch_code.clj
@@ -0,0 +1,23 @@
+(ns fetch-code
+  (:require [babashka.fs :as fs]
+            [babashka.tasks :as t]))
+
+(def proj-root (fs/cwd))
+
+(def repos-root 
+  (str proj-root "/repos"))
+
+(def repos-list
+  (str proj-root "/data/clojuredart-repos-list.txt"))
+
+(defn -main
+  [& _args]
+  (when (not (fs/exists? repos-root))
+    (fs/create-dir repos-root))
+  (with-open [rdr (clojure.java.io/reader repos-list)]
+    (doseq [url (line-seq rdr)]
+      (when (uri? (java.net.URI. url))
+        (when-let [[_ user name] 
+                   (re-matches #".*/([^/]+)/([^/]+)$" url)]
+          (let [dest-dir (str repos-root "/" name "." user)]
+            (t/shell (str "git clone " url " " dest-dir))))))))
---
+++ script/parse_tree.clj
@@ -0,0 +1,27 @@
+(ns parse-tree
+  (:require [babashka.fs :as fs]
+            [babashka.tasks :as t]
+            [clojure.string :as cs]))
+
+(def proj-root (fs/cwd))
+
+(def repos-root 
+  (str proj-root "/repos"))
+
+(defn -main
+  [& _args]
+  (when (fs/exists? repos-root)
+    (fs/walk-file-tree repos-root
+                       {:visit-file 
+                        (fn [path _]
+                          (when (cs/ends-with? (str path) ".cljd")
+                            (try
+                              (t/shell "tree-sitter parse --quiet " 
+                                       (str path))
+                              (catch Exception e
+                                (println "Exception:" (.getMessage e)))))
+                          :continue)})))
+
+
+
+
